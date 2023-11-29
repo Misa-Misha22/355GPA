6f3fe6071c4084d1467a52e290702c411c6685c5
misa-misha22 <madisonm2@winthrop.edu>
2023-11-29 15:19:58 -0500

    updated prep statements

--- movie.cpp
+++ movie.cpp
@@ -1,13 +1,25 @@
 
+#include <stdlib.h>
 #include <iostream>
+
+#include <mysql_connection.h>
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
 using namespace std;
+
 //#include //the SQL DB
 
-
+sql::Connection *con;
+sql::PreparedStatement *prep_stmt
 
 // function to create a movie
 // the database will go into the createMovie function
-void createMovie() {
+
+void insertMovie() {
     string name;
     string description;
     int yearReleased;
@@ -30,16 +42,29 @@ void createMovie() {
     cin.ignore(); // Ignore newline character from previous input
 
     // then insert into the table 
-
-    string insertStatementSQL = "INSERT INTO movie(name, yearReleased, runtime, description) VALUES (" + name + ", ", + yearReleased + ", " + runtime + "," + description + ");";
-
+    prep_stmt = con->PreparedStatement("INSERT INTO MOVIE(name, yearReleased, runtime, description) VALUES (?,?,?,?)");
     
+    prep_stmt->setString(1, name);
+    prep_stmt->setInt(2, yearReleased);
+    prep_stmt->setInt(3, runtiime)
+    prep_stmt->setString(4, description);
+
+    prep_stmt->execute();
+
+    delete prep_stmt;
+    delete con;
+
 }
 
 void updateMovie(){
     
     //create var for user input
     string userinput;
+
+    // prompt user for movie being updated
+
+    cout << "PLEASE ENTER ID:" <<endl;
+
 
     //do we update each individual? yes i think so 
     cout << "What do you want to update:" << endl;
@@ -69,6 +94,15 @@ void updateMovie(){
 
 }
 
+void deleteMovie(){
+
+
+}
+
+void readMovie(){
+
+    // select from
+}
 
 
 
