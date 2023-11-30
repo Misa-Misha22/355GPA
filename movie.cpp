169c75d0f1e164daf6dd1ce50fedb69769dc62b3
misa-misha22 <madisonm2@winthrop.edu>
2023-11-30 01:59:01 -0500

    i did alot idk

--- movie.cpp
+++ movie.cpp
@@ -12,7 +12,6 @@
 using namespace std;
 
 //#include //the SQL DB
-
 sql::Connection *con;
 sql::PreparedStatement *prep_stmt
 
@@ -54,54 +53,181 @@ void insertMovie() {
     delete prep_stmt;
     delete con;
 
+    cout << "All done ᕙ(▀̿ĺ̯▀̿ ̿)ᕗ" << endl;
+
 }
 
 void updateMovie(){
     
     //create var for user input
     string userinput;
-
+    string newName;
+    string newDscrip
+    string movieId;
+    string newYear;
+    string newRuntime;
     // prompt user for movie being updated
-
     cout << "PLEASE ENTER ID:" <<endl;
-
+    cin  >> movieId;
+    cin.ignore();
 
     //do we update each individual? yes i think so 
-    cout << "What do you want to update:" << endl;
+    cout << "What field do you want to update:" << endl;
     cout << "Name" << endl;
     cout << "Description" << endl;
-    cout << "yearReleased" << endl;
-    cout << "runtime" << endl;
+    cout << "YearReleased" << endl;
+    cout << "Runtime" << endl;
 
     cin >> userinput;
+    cin.ignore();
 
     if (userinput == "Name")
     {
+        cout << "ENTER NEW MOVIE NAME:" << endl;
+        cin >> newName;
+        cin.ignore(); // Ignore newline character from previous input
+
+        // then insert into the table 
+        prep_stmt = con->PreparedStatement("UPDATE MOVIE SET Name = ? WHERE id = ?");
+        
+        prep_stmt->setString(1, newName);
+        prep_stmt->setString(2, movieId);
+
+        prep_stmt->execute();
+
+        cout << "All done ᕙ(▀̿ĺ̯▀̿ ̿)ᕗ" << endl;
+
+        delete prep_stmt;
+        delete con;
 
     }
     else if (userinput == "Description")
     {
+        cout << "ENTER NEW MOVIE DESCRIPTION:" << endl;
+        cin >> newDscrip;
+        cin.ignore(); // Ignore newline character from previous input
 
+        // then insert into the table 
+        prep_stmt = con->PreparedStatement("UPDATE MOVIE SET Description = ? WHERE id = ?");
+        
+        prep_stmt->setString(1, newDscrip);
+        prep_stmt->setString(2, movieId);
+
+        prep_stmt->execute();
+
+        cout << "All done ᕙ(▀̿ĺ̯▀̿ ̿)ᕗ" << endl;
+
+        delete prep_stmt;
+        delete con;
     }
-    else if (userinput == "yearReleased")
+    else if (userinput == "YearReleased")
     {
+        cout << "ENTER NEW MOVIE YEAR RELEASED:" << endl;
+        cin >> newYear
+        cin.ignore(); // Ignore newline character from previous input
 
+        // then insert into the table 
+        prep_stmt = con->PreparedStatement("UPDATE MOVIE SET YearReleased = ? WHERE id = ?");
+        
+        prep_stmt->setString(1, newYear);
+        prep_stmt->setString(2, movieId);
+
+        prep_stmt->execute();
+
+        cout << "All done ᕙ(▀̿ĺ̯▀̿ ̿)ᕗ" << endl;
+
+        delete prep_stmt;
+        delete con;
     }
-    else if (userinput == "runtime")
+    else if (userinput == "Runtime")
     {
+        cout << "ENTER NEW MOVIE RUNTIME:" << endl;
+        cin >> newRuntime;;
+        cin.ignore(); // Ignore newline character from previous input
 
+        // then insert into the table 
+        prep_stmt = con->PreparedStatement("UPDATE MOVIE SET Runtime = ? WHERE id = ?");
+        
+        prep_stmt->setString(1, newRuntime;);
+        prep_stmt->setString(2, movieId);
+
+        cout << "All done ᕙ(▀̿ĺ̯▀̿ ̿)ᕗ" << endl;
+
+        prep_stmt->execute();
+
+        delete prep_stmt;
+        delete con;
     }
 
 }
 
 void deleteMovie(){
 
+    string userInput;
+    string movieId;
+
+    cout << "ENTER ID OF MOVIE TO DELETE: " << endl;
+    cin >> movieId;
+    cin.ignore(); // Ignore newline character from previous input
+
+        // then insert into the table 
+        prep_stmt = con->PreparedStatement("DELETE FROM Customers WHERE id = ?");
+        
+        prep_stmt->setString(1, movieId);
+        
+        int rowsDeleted = prep_stmt->executeUpdate();
+
+        if (rowsDeleted > 0) {
+            cout << "All done ᕙ(▀̿ĺ̯▀̿ ̿)ᕗ" << endl;
+        }
+        else {
+            cout << "No files deleted (ↁ_ↁ)" << endl;
+        }
+
+        // are we deleting one by one? or row by row?
+
+        delete prep_stmt;
+        delete con;
+    
 
 }
 
 void readMovie(){
 
     // select from
+    string userInput;
+    string movieId;
+
+    cout << "ENTER MOVIE ID: " << endl;
+    cin >> movieId;
+    cin.ignore(); // Ignore newline character from previous input
+
+        // then insert into the table 
+        prep_stmt = con->PreparedStatement("SELECT * FROM MOVIE WHERE id = ?");
+        
+        prep_stmt->setString(1, movieId);
+        
+        // ResultSet grabs the values that match the id
+        // executeQuery() executes the select statement and returns a result
+        sql::ResultSet *res = prep_stmt->executeQuery();
+
+        // we need a while loop to check the table for all variables and retreive them
+        while (res->next())
+        {
+            
+            cout << "ID: " << res->getInt("id") << endl;
+            cout << "Name: " << res->getString("Name") << endl;
+            cout << "Year Released: " << res->getInt("YearReleased") << endl;
+            cout << "Runtime: " << res->getInt("Runtime") << endl;
+            cout << "Description: " << res->getString("Description") << endl;
+
+        }
+
+        delete res;
+        delete pstmt;
+        delete con;
+
+        return 0;
 }
 
 
@@ -110,29 +236,35 @@ int main()
 {
 
     //create a var for user input
-    string userinput;
+    string userInput;
 
     // prompt the user for what they want to do
     cout << "What do you want to do:" << endl;
-    cout << "Create" << endl;
+    cout << "Insert" << endl;
     cout << "Read" << endl;
     cout << "Update" << endl;
     cout << "Delete" << endl;
 
     cin >> userinput;
 
-    while (userinput != "Create" || "Read" || "Update" || "Delete") 
+    while ((userInput != "Insert") || (userInput != "Read") || (userInput != "Update") || (userInput != "Delete"))
     {
         cout << "CASE SENSITIVE PLEASE TYPE OPTION EXACTLY" << endl;
-
         cout << "What do you want to do:" << endl;
+        cout << "Insert" << endl;
+        cout << "Read" << endl;
+        cout << "Update" << endl;
+        cout << "Delete" << endl;
+
+        // prompt user again
+        cin >> userInput; 
     }
 
 
-    if (userinput == "Create")
+    if (userinput == "Insert")
     {
-        //call function createMovie within database 
-        createMovie()
+        //call function insertMovie within database 
+        insertMovie()
     }
 
 
